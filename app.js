d42c1b2c2f365288dcba8f5b125cfe7f5766ab4d
Christoph Ruepprich <crueppricgh@gmail.com>
2018-04-03 19:44:50 -0500

    sensor working with blynk

--- app.js
+++ app.js
@@ -1,40 +1,44 @@
 //https://www.npmjs.com/package/blynk-library
 const BlynkLib = require('blynk-library');
+const sensor = require('node-dht-sensor');
  
 const blynk = new BlynkLib.Blynk('b42be0df478345648e0c93f9e3df1fdd');
-var v1 = new blynk.VirtualPin(1);
+
 var v9 = new blynk.VirtualPin(9);
 let txt, rnd
 
-v1.on('write', function(param) {
-  blynk.virtualWrite(9,1*param);
-  if (param == 0 ){
-    txt = '----'
-  } else {
-    txt = 'Hello There'
-  }
-
-  blynk.virtualWrite(0,txt);
-  blynk.virtualWrite(2,200*param);
-  bumpVal()
-});
-
-v1.on('read', function(param) {
-  console.log('V1: read', param);
-});
-
-
 
 v9.on('write', function() {
-  bumpVal()
+  getSensor()
+    .then((data)=>{
+      const out = "Temp: "+data.tempF+"  Hum: "+data.hum
+      console.log('data',out)
+      blynk.virtualWrite(0,out)
+      blynk.virtualWrite(3,data.hum)
+      blynk.virtualWrite(4,data.tempF)
+    });
 });
 
 
-const bumpVal = () => {
 
-  rnd = Math.round(Math.random() *(100-0)+0,0)
-  console.log('V9: bump', rnd);
-  blynk.virtualWrite(3,rnd);
+
+
+const getSensor = () => {
+  return new Promise((resolve,reject) => {
+    let sensorOut,tempF
+  
+    sensor.read(22, 4, function(err, temperature, humidity) {
+      if (!err) {
+          tempF = temperature*9/5+32
+          sensorOut = {"tempC":temperature.toFixed(),"tempF":tempF.toFixed(),"hum":humidity.toFixed()}
+          
+          resolve(sensorOut)
+      } else {
+          reject(err)
+      }
+    });
+  })
 }
+
 
 blynk.syncVirtual()
